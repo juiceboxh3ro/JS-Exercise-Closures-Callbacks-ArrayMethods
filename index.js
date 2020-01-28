29ff82a641e6891e4edc9e4f91c0d46e5f920d47
Jesse <fkng3rmz@gmail.com>
2020-01-27 20:48:38 -0500

    I finished all the non-stretch problems?

--- index.js
+++ index.js
@@ -140,8 +140,12 @@ function processProduct(num1, num2, callback) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
 */
-function processContains(/* CODE HERE */) {
-  /* CODE HERE */
+function processContains(item, list, callback) {
+  let yes = false;
+  if(list.includes(item)) {
+    yes = true;
+  }
+  return(callback(yes));
 }
 
 /**
